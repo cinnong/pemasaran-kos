cc74587a4901bb8b41444d29a2487575f5839874
Dina Oktafiani <dinaoktafiani04@gmail.com>
2024-06-22 15:57:42 +0700

    ganti variabel $kos jadi $datakos di search-by-location

--- resources/views/datakos/search-by-location.blade.php
+++ resources/views/datakos/search-by-location.blade.php
@@ -7,11 +7,11 @@
             <p class="text-gray-600">Tidak ada hasil yang ditemukan.</p>
         @else
             <ul>
-                @foreach ($datakos as $kosan)
+                @foreach ($datakos as $datakos)
                     <li class="mb-4 p-4 bg-white rounded-lg shadow">
-                        <h2 class="text-xl font-semibold">{{ $kosan->nama }}</h2>
-                        <p>{{ $kosan->lokasi }}</p>
-                        <p>{{ $kosan->harga }}</p>
+                        <h2 class="text-xl font-semibold">{{ $datakos->nama }}</h2>
+                        <p>{{ $datakos->lokasi }}</p>
+                        <p>{{ $datakos->harga }}</p>
                         <!-- Tambahkan detail lainnya sesuai kebutuhan -->
                     </li>
                 @endforeach
