c2ac8475e68fa7ec7ee2a9ab22d4c476221f2690
Dina Oktafiani <dinaoktafiani04@gmail.com>
2024-07-17 13:43:15 +0700

    gedein tulisannya

--- resources/views/datakos/show.blade.php
+++ resources/views/datakos/show.blade.php
@@ -17,32 +17,32 @@
                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
             </div>
             <div class="w-full lg:w-1/2 lg:pl-8 mt-6 lg:mt-0">
-                <h1 class="text-2xl font-bold mb-4">KOS</h1>
-                <p><strong>Nama:</strong> {{ $datakos->nama }}</p>
-                <p><strong>Lokasi:</strong> {{ $datakos->lokasi }}</p>
-                <p><strong>Harga:</strong> {{ $datakos->harga }}</p>
-                <p><strong>Jumlah Kamar:</strong> {{ $datakos->jumlah_kamar }}</p>
-                <p><strong>Kamar Terisi:</strong> {{ $kamarTerisi }}</p>
-                <p><strong>Tipe Kos:</strong> {{ ucfirst(str_replace('_', ' ', $datakos->tipekos)) }}</p>
-                <p class="overflow-wrap break-word"><strong>Deskripsi:</strong> {{ $datakos->deskripsi }}</p>
-                <p><strong>No. Telepon:</strong> {{ $datakos->notlp }}</p>
+                {{-- <h1 class="text-3xl font-bold mb-4">KOS</h1> --}}
+                <p class="text-xl"><strong>Nama:</strong> {{ $datakos->nama }}</p>
+                <p class="text-xl"><strong>Lokasi:</strong> {{ $datakos->lokasi }}</p>
+                <p class="text-xl"><strong>Harga:</strong> {{ $datakos->harga }}</p>
+                <p class="text-xl"><strong>Jumlah Kamar:</strong> {{ $datakos->jumlah_kamar }}</p>
+                <p class="text-xl"><strong>Kamar Terisi:</strong> {{ $kamarTerisi }}</p>
+                <p class="text-xl"><strong>Tipe Kos:</strong> {{ ucfirst(str_replace('_', ' ', $datakos->tipekos)) }}
+                </p>
+                <p class="text-xl overflow-wrap break-word"><strong>Deskripsi:</strong> {{ $datakos->deskripsi }}</p>
+                <p class="text-xl"><strong>No. Telepon:</strong> {{ $datakos->notlp }}</p>
                 <a href="{{ route('beranda') }}">
-                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl">
                         Back
                     </button>
                 </a>
                 <a href="tel:{{ $datakos->notlp }}">
-                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl">
                         Hubungi Pemilik
                     </button>
                 </a>
                 <a href="{{ route('pemesanan.pesan', ['datakos_id' => $datakos->id]) }}" target="_blank">
-                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl">
                         Pesan
                     </button>
                 </a>
             </div>
-
         </div>
     </div>
 </body>
